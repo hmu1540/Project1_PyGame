0c7a2ffcce36f1da87287a0a813ca624cd626765
Huimin <huimin_mu1@baylor.edu>
2021-09-22 12:17:54 -0500

    add initial py file

---
+++ alien_invasion.py
@@ -0,0 +1,51 @@
+import sys  # ???????????
+
+import pygame
+
+
+class AlienInvasion:
+    """Overall calss to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()  # Initialize the background settings
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        """ 
+        RHS Creates a display window 1200 pi * 800 pi
+        RHS is called a surface: part of a screen for displaying a game element
+        Each element is its own surface, like an alien or a ship 
+        display.set_mode() represents the entire game window, redrawn in loop
+        """
+
+        pygame.display.set_caption("Alien Invasion")
+
+        # 2. Set the background color
+        self.bg_color = (
+            230,
+            230,
+            230,
+        )  # ????????why not called some method on display?
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                # action that user performs, eg. press a key, move the mouse.
+                # return a list of events since last time get() called
+                #
+                if event.type == pygame.QUIT:  # click the game wind close button
+                    sys.exit()  # exit the game
+
+            # Redrawn the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()  # disply most recently drawn game window, update continually
+
+
+if __name == "__main__":
+    # Make a game instance, and run the game
+    ai = AlienInvasion()
+    ai.run_game()
