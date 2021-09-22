d78e31a647f839a58b79d20b7c918f958797768c
Huimin <huimin_mu1@baylor.edu>
2021-09-22 13:57:15 -0500

    modify Ship class

--- ship.py
+++ ship.py
@@ -14,5 +14,20 @@ class Ship:
         )  # haven't defined this method inside AlienInvasion????????
 
         # Load the ship image and get its rect.
-        self.image = pygame.image.load("images/ship.bmp")
-        self.rect = self.image.get_rect()
+        self.image = pygame.image.load(
+            "images/ship.bmp"
+        )  # returns a surface representing the ship
+        self.rect = (
+            self.image.get_rect()
+        )  # access the ship surface’s rect attribute so we can later use it to place the ship. return a rect object that has attributes: left right top bottom center centerx mid-
+
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = (
+            self.screen_rect.midbottom
+        )  # image midbottom location is screen_rect midbottom loc
+
+        def blitme(self):
+            """Draw the ship at its current location."""
+            self.screen.blit(
+                self.image, self.rect
+            )  # draws the image to the screen at the position specified by self.rect
