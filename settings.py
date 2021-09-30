3a4793c436a90991dccf8bc9f2fd708840bcf5fa
Huimin <huimin_mu1@baylor.edu>
2021-09-29 21:14:08 -0500

    Add image related attributes

--- settings.py
+++ settings.py
@@ -8,3 +8,5 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
+        self.image_width = 120
+        self.image_height = 60
