9412ed9339233606125aae97d7aff285972f4fb3
Huimin <huimin_mu1@baylor.edu>
2021-10-04 17:05:39 -0500

    Respond to a Keypress

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,11 +1,7 @@
 import sys  # ???????????
-
 import pygame
-
 from settings import Settings
-
 from ship import Ship
-from superman import Superman
 
 
 class AlienInvasion:
@@ -14,13 +10,13 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()  # Initialize the background settings
-        self.settings = (
-            Settings()
-        )  # attribute settings is an instance of Settings object, so it has attributs, methods
+        self.settings = Settings()
+        # attribute settings is an instance of Settings object, so it has attributs, methods
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height)
         )  # don't think this is a good way, access attributes directly instead of via methods
+
         """ 
         RHS Creates a display window 1200 pi * 800 pi
         RHS is called a surface: part of a screen for displaying a game element
@@ -32,14 +28,6 @@ class AlienInvasion:
         self.ship = Ship(
             self
         )  # e call to Ship() requires one argument, an instance of AlienInvasion
-
-        """         # 2. Set the background color
-                self.bg_color = (
-                    230,
-                    230,
-                    230,
-                )  # ????????why not called some method on display? """
-        self.superman = Superman(self)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -56,7 +44,8 @@ class AlienInvasion:
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
             # Make the most recently drawn screen visible.
-            pygame.display.flip()  # disply most recently drawn game window, update continually """
+            pygame.display.flip() 
+            # disply most recently drawn game window, update continually """
 
             self._check_events()  # refactoring to simplify...use a helper method inside a class, not called by an instance
             self._update_screen()
@@ -65,12 +54,18 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.key == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.rect.x += 1
 
     def _update_screen(self):
         """update ... and flip ..."""
         self.screen.fill(self.settings.bg_color)
-        # self.ship.blitme()
+        self.ship.blitme()
+        """ self.superman.scale()
+        self.superman.set_color()
         self.superman.blitme()
+        """
         pygame.display.flip()
 
 
