aadb57bc4ca6ca3902487b727388de59bc933d93
Huimin <huimin_mu1@baylor.edu>
2021-10-11 12:50:05 -0500

    Add the bullet settings

--- settings.py
+++ settings.py
@@ -9,3 +9,9 @@ class Settings:
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.5
+
+        # bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3  # pixels
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)  # dark grey
